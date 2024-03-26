888d9a7b2a7d39607079e644391feb0b5b697e86
james0800 <160604211+james0800@users.noreply.github.com>
2024-03-26 11:09:11 +0000

    3212

---
+++ 0321/lib/A.py
@@ -0,0 +1,19 @@
+
+from pprint import pprint
+import random
+import MC2
+import pyinputplus as pyip
+
+num:int=pyip.inputInt()
+names:list[str]=MC2.f(num)
+st:list=[]
+for i in names:
+    stu:dict={}
+    stu["name"]=i
+    stu["height"]=random.randint(150,220)
+    
+    
+    stu["weight"]=random.randint(40,90)
+    
+    st.append(stu)
+print(st)
--- 0321/lib/MC2.py
+++ 0321/lib/MC2.py
@@ -1,8 +1,21 @@
 import random
 def f(num:int):
-    with open("names.txt",encoding="utf8") as file:
+    
+    with open("names.txt") as file:
         con=file.read()
         arr:list[str]=con.split('\n')
         name=random.choices(arr,k=num)
-        return name
         file.close()
+        return name
+names:list[str]=f(5)
+st:list=[]
+for i in names:
+    stu:dict={}
+    stu["name"]=i
+    stu["height"]=random.randint(150,220)
+    
+    
+    stu["weight"]=random.randint(40,90)
+    
+    st.append(stu)
+print(st)
