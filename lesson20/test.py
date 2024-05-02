8a56dacbefb18ec11b8660a9538aa32cf59440c9
James0800 <011016mlaivs@gmail.com>
2024-05-02 14:24:49 +0000

    fsfs

--- lesson20/test.py
+++ lesson20/test.py
@@ -21,7 +21,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    #message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text="啾啾啾")
     line_bot_api.reply_message(event.reply_token, message)
     
 import os
